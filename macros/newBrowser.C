1e212fb1acc7d94c32967045a64cb0d46183115c
Alex Pearce <alex@alexpearce.me>
2014-06-26 22:20:06 +0100

    Update ROOT macro for ROOT 6 compatibility.

--- macros/newBrowser.C
+++ macros/newBrowser.C
@@ -1,3 +1,15 @@
+void newBrowser();
+int main(int argc, char *argv[]);
+
+// Open a TBrowser
+// Use like
+//   $ root -l file1.root file2.root ... newBrowser.C
 void newBrowser() {
-  new TBrowser;
+  char *argv = new char[0];
+  main(0, &argv);
+}
+
+int main(int argc, char *argv[]) {
+  TBrowser *browser = new TBrowser;
+  return 0;
 }
