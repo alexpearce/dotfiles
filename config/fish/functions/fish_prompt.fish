3455d3180ec7d1dd754e27fd8336d091b8a2463d
Alex Pearce <alex@alexpearce.me>
2015-11-05 09:34:36 +0100

    Show user/host information in SSH sessions.

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,16 @@
 function fish_prompt -d "Set prompt with pwd and git information"
   echo
+  # Show username@hostname if this is an SSH session
+  if test -n "$SSH_CLIENT" -o -n "$SSH_TTY"
+    set_color red
+    printf '%s' (whoami)
+    set_color normal
+    printf '@'
+    set_color purple
+    printf '%s' (hostname)
+    set_color normal
+    printf ' '
+  end
   set_color blue
   printf '%s' (pwd | sed -e "s|$HOME|~|")
   set_color normal
