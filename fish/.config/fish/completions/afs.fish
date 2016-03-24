2b258448445ef32db345b815293ba4cc3d41c69b
Alex Pearce <alex@alexpearce.me>
2016-03-24 14:00:11 +0100

    Better AFS command.
    Now autocompletes nested directories, rather than only the top-level
    ones.

--- fish/.config/fish/completions/afs.fish
+++ fish/.config/fish/completions/afs.fish
@@ -1,8 +1,13 @@
 set AFSHOME '/afs/cern.ch/user/a/apearce/'
 set AFSWORK '/afs/cern.ch/work/a/apearce/'
 
-complete -f -c afs -n '__fish_prog_needs_command afs' -a home
-complete -c afs -n '__fish_prog_using_command home' -a '(__fish_complete_directories $AFSHOME | sed -e "s#$AFSHOME##")'
+# Takes first argument as one of home or work
+complete --command afs --arguments 'home work' --authoritative
 
-complete -f -c afs -n '__fish_prog_needs_command afs' -a work
-complete -c afs -n '__fish_prog_using_command work' -a '(__fish_complete_directories $AFSWORK | sed -e "s#$AFSWORK##")'
+complete --command afs --exclusive \
+  --condition '__fish_prog_using_command home' \
+  --arguments '(__fish_complete_directories $AFSHOME/(commandline -ct) | sed -e "s#$AFSHOME##")'
+
+complete --command afs --exclusive \
+  --condition '__fish_prog_using_command work' \
+  --arguments '(__fish_complete_directories $AFSWORK/(commandline -ct) | sed -e "s#$AFSWORK##")'
