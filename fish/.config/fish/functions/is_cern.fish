383276d02c4704ed4cf43aebb3902657227472fd
Alex Pearce <alex@alexpearce.me>
2016-04-21 09:44:03 +0200

    Support my VMs in is_cern check.

--- fish/.config/fish/functions/is_cern.fish
+++ fish/.config/fish/functions/is_cern.fish
@@ -2,5 +2,5 @@ function is_cern -d "Test whether host is a CERN machine"
   # Most CERN machines have a hostname ending in cern.ch
   test (hostname | grep -E cern\.ch)
   # But not the ones in the LHCb online network or my VMs
-  or test (hostname | grep -E "(gw|plus)[0-9]*")
+  or test (hostname | grep -E "(gw|plus|apv)[0-9]*")
 end
