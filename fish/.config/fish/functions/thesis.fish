4399c60a6c2d9a5db56356bd643d6d322d1a2b14
Alex Pearce <alex@alexpearce.me>
2016-08-22 09:16:47 +0200

    Fish function for setting up thesis-writing session.

---
+++ fish/.config/fish/functions/thesis.fish
@@ -0,0 +1,17 @@
+function thesis -d "Set up tmux session for thesis writing"
+  cd ~/Documents/LHCb/Thesis
+  # Vertical split
+  tmux split-window -v
+  # Select the bottom pane
+  tmux select-pane -t 1
+  # Shrink the bottom pane by 'some amount'
+  tmux resize-pane -D 20
+  # Open the PDF viewier
+  tmux send-keys "open thesis.pdf" C-m
+  # Start the watcher to automatically recompile when a .tex file changes
+  tmux send-keys "fswatch -0 **/*.tex | xargs -0 -n1 -I\"{}\" ./latexrun --latex-cmd=lualatex thesis.tex" C-m
+  # Select the top pane
+  tmux select-pane -t 0
+  # Open ctrl-p in vim
+  tmux send-keys C-p
+end
