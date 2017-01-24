7cf95d9fdb295114d167c5787825f59423cb4330
Alex Pearce <alex@alexpearce.me>
2017-01-24 20:34:46 +0100

    Add some more paths for CERN machines.

--- fish/.config/fish/env_cern.fish
+++ fish/.config/fish/env_cern.fish
@@ -1,3 +1,4 @@
 # Shell set up specifically for CERN
-set PATH ~/usr/bin $PATH
+set PATH ~/usr/bin ~/.local/bin /afs/cern.ch/sw/XML/texlive/latest/bin/x86_64-linux $PATH
+set PATH $PATH /cvmfs/lhcb.cern.ch/lib/contrib/ninja/1.4.0/x86_64-slc6-gcc48-opt
 set -x LD_LIBRARY_PATH ~/usr/lib:$LD_LIBRARY_PATH
