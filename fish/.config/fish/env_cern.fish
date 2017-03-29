6a44d6e7985c10650dd794ffb9663ec5c1601a5f
Alex Pearce <alex.pearce@cern.ch>
2017-03-29 10:34:05 +0200

    Parallel ninja builds.

--- fish/.config/fish/env_cern.fish
+++ fish/.config/fish/env_cern.fish
@@ -2,3 +2,5 @@
 set PATH ~/usr/bin ~/.local/bin /afs/cern.ch/sw/XML/texlive/latest/bin/x86_64-linux $PATH
 set PATH $PATH /cvmfs/lhcb.cern.ch/lib/contrib/ninja/1.4.0/x86_64-slc6-gcc48-opt
 set -x LD_LIBRARY_PATH ~/usr/lib:$LD_LIBRARY_PATH
+
+set NINJAFLAGS -j6
