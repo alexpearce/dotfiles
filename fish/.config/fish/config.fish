fae47bc2debcf38de09c42c3747597e4ab757700
Alex Pearce <alex@alexpearce.me>
2017-05-04 09:11:15 +0200

    Use iTerm tmux integration.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 # Start tmux automatically if we're not connecting over SSH,
 # attaching to an existing session if possible
 if not set -q TMUX; and not is_ssh
-  tmux attach ^/dev/null; or tmux
+  tmux -CC attach ^/dev/null; or tmux -CC
 end
 
 set -l source_dir (dirname (status -f))
