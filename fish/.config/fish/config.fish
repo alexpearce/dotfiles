a4c0e9e745343224773c69d1068800115eaae626
Alex Pearce <alex@alexpearce.me>
2020-02-10 09:51:15 +0100

    Add auto Kerberos token renewal.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -14,4 +14,5 @@ set -l source_dir (dirname (status -f))
 source $source_dir/env.fish
 source $source_dir/aliases.fish
 source $source_dir/git.fish
+source $source_dir/kerberos.fish
 source $source_dir/iterm2_shell_integration.fish
