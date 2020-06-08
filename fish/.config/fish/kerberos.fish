6ab00f171b8d7f483b0b8eaec6be638ab99d9a1a
Alex Pearce <alex@alexpearce.me>
2020-06-08 08:32:11 +0200

    Clean up Kerberos command.

--- fish/.config/fish/kerberos.fish
+++ fish/.config/fish/kerberos.fish
@@ -1,10 +1,10 @@
 # Auto-generate a Kerberos token when needed, if possible
 # Place a keytab file at $HOME/.$USER.keytab to enable
 set -l keytab_path $HOME/.$USER.keytab
-if not klist -t
+if not klist -t ^ /dev/null
   if not test -f "$keytab_path"
     echo "🔓 You do not have a Kerberos token and I couldn't find a keytab at $keytab_path"
   else
-    kinit -kt "$keytab_path"
+    kinit -l 24h -kt "$keytab_path"
   end
 end
