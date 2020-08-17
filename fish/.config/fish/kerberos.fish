898a91a5d60167a19445504221ace6fdc60841d7
Alex Pearce <alex@alexpearce.me>
2020-08-17 08:42:40 +0200

    Use macOS/Linux compatible flag.

--- fish/.config/fish/kerberos.fish
+++ fish/.config/fish/kerberos.fish
@@ -1,7 +1,7 @@
 # Auto-generate a Kerberos token when needed, if possible
 # Place a keytab file at $HOME/.$USER.keytab to enable
 set -l keytab_path $HOME/.$USER.keytab
-if not klist -t ^ /dev/null
+if not klist -s ^ /dev/null
   if not test -f "$keytab_path"
     echo "🔓 You do not have a Kerberos token and I couldn't find a keytab at $keytab_path"
   else
