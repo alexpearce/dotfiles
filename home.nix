42d34da380987915d1f822b489f24cfb7c2c952c
Alex Pearce <alex@alexpearce.me>
2023-09-09 15:39:20 +0100

    Pearce to Pearwin.

--- home.nix
+++ home.nix
@@ -199,8 +199,8 @@
 
     git = {
       enable = true;
-      userName = "Alex Pearce";
-      userEmail = "alex@alexpearce.me";
+      userName = "Alex Pearwin";
+      userEmail = "alex@pearwin.com";
       aliases = {
         prettylog = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
       };
