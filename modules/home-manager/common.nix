f7fd9b817d90437da23ea3724f415c581a7e4e41
Alex Pearwin <alex@pearwin.com>
2025-10-31 06:52:42 +0000

    Typo

--- modules/home-manager/common.nix
+++ modules/home-manager/common.nix
@@ -214,12 +214,6 @@
         alias = {
           prettylog = "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
         };
-        core = {
-          # If git uses `ssh` from Nix the macOS-specific configuration in
-          # `~/.ssh/config` won't be seen as valid
-          # https://github.com/NixOS/nixpkgs/issues/15686#issuecomment-865928923
-          sshCommand = "/usr/bin/ssh";
-        };
         color = {
           ui = true;
         };
